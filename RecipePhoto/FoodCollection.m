5781b0aea72f6fa55b0c6775bdfea794a07205ba
pietbinon <pierrebinon@icloud.com>
2017-03-21 11:57:52 -0400

    New minor improvements - Mar 21, 2017

--- RecipePhoto/FoodCollection.m
+++ RecipePhoto/FoodCollection.m
@@ -15,26 +15,51 @@
 + (NSArray *) returnArrayOfFoods {
     
 //    return @[
-//             [[Food alloc] initWithDescription: @"hamburger" andImage: [UIImage imageNamed: @"hamburger.jpg"]],
-//             [[Food alloc] initWithDescription: @"risotto" andImage: [UIImage imageNamed: @"mushroom_risotto.jpg"]],
-//             [[Food alloc] initWithDescription: @"curry" andImage: [UIImage imageNamed: @"vegetable_curry.jpg"]],
-//             [[Food alloc] initWithDescription: @"angry_birds_cake" andImage: [UIImage imageNamed: @"angry_birds_cake.jpg"]];
-//             [[Food alloc] initWithDescription: @"creme_brelee" andImage: [UIImage imageNamed: @"creme_brelee.jpg"]];
-//             [[Food alloc] initWithDescription: @"starbucks_coffee" andImage: [UIImage imageNamed: @"starbucks_coffee.jpg"]]];
+//             [[Food alloc] initWithDescription: @"Hamburger" andImage: [UIImage imageNamed: @"hamburger.jpg"]],
+//             [[Food alloc] initWithDescription: @"Risotto" andImage: [UIImage imageNamed: @"mushroom_risotto.jpg"]],
+//             [[Food alloc] initWithDescription: @"Curry" andImage: [UIImage imageNamed: @"vegetable_curry.jpg"]],
+//             [[Food alloc] initWithDescription: @"Angry Birds Cake" andImage: [UIImage imageNamed: @"angry_birds_cake.jpg"]],
+//             [[Food alloc] initWithDescription: @"Creme Brulee" andImage: [UIImage imageNamed: @"creme_brelee.jpg"]],
+//             [[Food alloc] initWithDescription: @"Starbucks Coffee" andImage: [UIImage imageNamed: @"starbucks_coffee.jpg"]],
+//             [[Food alloc] initWithDescription: @"Eggs Benedicte" andImage: [UIImage imageNamed: @"egg_benedict.jpg"]],
+//             [[Food alloc] initWithDescription: @"Full Breakfast" andImage: [UIImage imageNamed: @"full_breakfast.jpg"]],
+//             [[Food alloc] initWithDescription: @"Green Tea" andImage: [UIImage imageNamed: @"green_tea.jpg"]],
+//             [[Food alloc] initWithDescription: @"Ham & Cheese Panini" andImage: [UIImage imageNamed: @"ham_and_cheese_panini.jpg"]],
+//             [[Food alloc] initWithDescription: @"Ham & Cheese Sandwich" andImage: [UIImage imageNamed: @"ham_and_egg_sandwich.jpg"]],
+//             [[Food alloc] initWithDescription: @"Instant Noodle With Egg" andImage: [UIImage imageNamed: @"instant_noodle_with_egg.jpg"]],
+//             [[Food alloc] initWithDescription: @"Japanese Noodle With Pork" andImage: [UIImage imageNamed: @"japanese_noodle_with_pork.jpg"]],
+//             [[Food alloc] initWithDescription: @"Noodle with Barbecue Pork" andImage: [UIImage imageNamed: @"noodle_with_bbq_pork.jpg"]],
+//             [[Food alloc] initWithDescription: @"Thai Shrimo Cake" andImage: [UIImage imageNamed: @"thai_shrimp_cake.jpg"]],
+//             [[Food alloc] initWithDescription: @"White Chocolate Donut" andImage: [UIImage imageNamed: @"white_chocolate_donut.jpg"]]];
+
+
+
 
     
-    Food *foodA = [[Food alloc] initWithDescription: @"hamburger" andImage: [UIImage imageNamed: @"hamburger.jpg"]];
-    Food *foodB = [[Food alloc] initWithDescription: @"risotto" andImage: [UIImage imageNamed: @"mushroom_risotto.jpg"]];
-    Food *foodC = [[Food alloc] initWithDescription: @"curry" andImage: [UIImage imageNamed: @"vegetable_curry.jpg"]];
+    Food *foodA = [[Food alloc] initWithDescription: @"Hamburger" andImage: [UIImage imageNamed: @"hamburger.jpg"]];
+    Food *foodB = [[Food alloc] initWithDescription: @"Risotto" andImage: [UIImage imageNamed: @"mushroom_risotto.jpg"]];
+    Food *foodC = [[Food alloc] initWithDescription: @"Curry" andImage: [UIImage imageNamed: @"vegetable_curry.jpg"]];
+    Food *foodD = [[Food alloc] initWithDescription: @"Eggs Benedicte" andImage: [UIImage imageNamed: @"egg_benedict.jpg"]];
+    Food *foodE = [[Food alloc] initWithDescription: @"Full Breakfast" andImage: [UIImage imageNamed: @"full_breakfast.jpg"]];
+    Food *foodF = [[Food alloc] initWithDescription: @"Ham & Cheese Panini" andImage: [UIImage imageNamed: @"ham_and_cheese_panini.jpg"]];
+    Food *foodG = [[Food alloc] initWithDescription: @"Ham & Cheese Sandwich" andImage: [UIImage imageNamed: @"ham_and_egg_sandwich.jpg"]];
+    Food *foodH = [[Food alloc] initWithDescription: @"Instant Noodle With Egg" andImage: [UIImage imageNamed: @"instant_noodle_with_egg.jpg"]];
+    Food *foodI = [[Food alloc] initWithDescription: @"Japanese Noodle With Pork" andImage: [UIImage imageNamed: @"japanese_noodle_with_pork.jpg"]];
+    Food *foodJ = [[Food alloc] initWithDescription: @"Noodle with Barbecue Pork" andImage: [UIImage imageNamed: @"noodle_with_bbq_pork.jpg"]];
     
-    NSArray *mainDish = @[foodA, foodB, foodC];
+    NSArray *mainDish = @[foodA, foodB, foodC, foodD, foodE, foodF, foodG, foodH, foodI, foodJ];
 
     
-    Food *foodD = [[Food alloc] initWithDescription: @"angry_birds_cake" andImage: [UIImage imageNamed: @"angry_birds_cake.jpg"]];
-    Food *foodE = [[Food alloc] initWithDescription: @"creme_brelee" andImage: [UIImage imageNamed: @"creme_brelee.jpg"]];
-    Food *foodF = [[Food alloc] initWithDescription: @"starbucks_coffee" andImage: [UIImage imageNamed: @"starbucks_coffee.jpg"]];
+    Food *foodK = [[Food alloc] initWithDescription: @"Angry Birds Cake" andImage: [UIImage imageNamed: @"angry_birds_cake.jpg"]];
+    Food *foodL = [[Food alloc] initWithDescription: @"Creme Brulee" andImage: [UIImage imageNamed: @"creme_brelee.jpg"]];
+    Food *foodM = [[Food alloc] initWithDescription: @"Starbucks Coffee" andImage: [UIImage imageNamed: @"starbucks_coffee.jpg"]];
+    Food *foodN = [[Food alloc] initWithDescription: @"Green Tea" andImage: [UIImage imageNamed: @"green_tea.jpg"]];
+    Food *foodO = [[Food alloc] initWithDescription: @"Thai Shrimo Cake" andImage: [UIImage imageNamed: @"thai_shrimp_cake.jpg"]];
+    Food *foodP = [[Food alloc] initWithDescription: @"White Chocolate Donut" andImage: [UIImage imageNamed: @"white_chocolate_donut.jpg"]];
+
+
     
-    NSArray *dessertDish = @[foodD, foodE, foodF];
+    NSArray *dessertDish = @[foodK, foodL, foodM, foodN, foodO, foodP];
     
     
     return [NSArray arrayWithObjects: mainDish, dessertDish, nil];
--- RecipePhoto/RecipeCollectionViewController.m
+++ RecipePhoto/RecipeCollectionViewController.m
@@ -36,11 +36,13 @@
     [super viewDidLoad];
     
     self.foods = [FoodCollection returnArrayOfFoods];
-    
+
+//=========================================================================================================================================================
 //    self.foods = [NSArray arrayWithObjects:@"angry_birds_cake.jpg", @"creme_brelee.jpg", @"egg_benedict.jpg", @"full_breakfast.jpg", @"green_tea.jpg", @"ham_and_cheese_panini.jpg", @"ham_and_egg_sandwich.jpg", @"hamburger.jpg", @"instant_noodle_with_egg.jpg", @"japanese_noodle_with_pork.jpg", @"mushroom_risotto.jpg", @"noodle_with_bbq_pork.jpg", @"starbucks_coffee.jpg", @"thai_shrimp_cake.jpg", @"vegetable_curry.jpg", @"white_chocolate_donut.jpg", nil];
     
     
 //    recipeImages = [NSArray arrayWithObjects:@"angry_birds_cake.jpg", @"creme_brelee.jpg", @"egg_benedict.jpg", @"full_breakfast.jpg", @"green_tea.jpg", @"ham_and_cheese_panini.jpg", @"ham_and_egg_sandwich.jpg", @"hamburger.jpg", @"instant_noodle_with_egg.jpg", @"japanese_noodle_with_pork.jpg", @"mushroom_risotto.jpg", @"noodle_with_bbq_pork.jpg", @"starbucks_coffee.jpg", @"thai_shrimp_cake.jpg", @"vegetable_curry.jpg", @"white_chocolate_donut.jpg", nil];
+//=========================================================================================================================================================
 }
 
 
@@ -67,6 +69,8 @@
          // [self.collectionView deselectItemAtIndexPath:indexPath animated:NO];
      }
  }
+
+
 
 
 #pragma mark <UICollectionViewDataSource>
